baaf8e38ee737970fb1463db3ba0c5101b790c5b
Екатерина Копылова <ek53084@gmail.com>
2023-05-02 05:47:16 +0900

    Сделана авторизация

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -14,10 +14,43 @@ class Controlers {
     });
   }
   getAccess(req, res) {
-    db.query(`SELECT user.email, course.title FROM user INNER JOIN user_course ON user.id = user_course.id_user INNER JOIN course ON user_course.id_course = course.id WHERE user.email = '${req.body.email}'`, (err, result) => {
-      console.log(result);
-      res.json(result);
-    });
+    db.query(
+      `SELECT user.email, course.title FROM user INNER JOIN user_course ON user.id = user_course.id_user INNER JOIN course ON user_course.id_course = course.id WHERE user.email = '${req.body.email}'`,
+      (err, result) => {
+        console.log(result);
+        res.json(result);
+      }
+    );
+  }
+  checkLogin(req, res) {
+    db.query(
+      `SELECT * FROM user WHERE user.email = "${req.body.email}" AND user.password = "${req.body.password}"`,
+      (err, result) => {
+        if (result.length === 0) {
+          console.log("Ничего не найдено");
+          res.json({ message: "Ошибка" });
+        } else {
+          delete result[0]["password"];
+          console.log(result[0]);
+          res.json(result[0]);
+        }
+      }
+    );
+  }
+  getUser(req, res) {
+    db.query(
+      `SELECT * FROM user WHERE user.id = "${req.body.id}"`,
+      (err, result) => {
+        if (result.length === 0) {
+          console.log("Ничего не найдено");
+          res.json({ message: "Ошибка" });
+        } else {
+          delete result[0]["password"];
+          console.log(result[0]);
+          res.json(result[0]);
+        }
+      }
+    );
   }
 }
 
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
 const controllers = require("./controllers/controllers.js");
+const cors = require("cors");
+
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.get("/users", controllers.getUsers);
 app.get("/courses", controllers.getCourses);
 app.post("/access", controllers.getAccess);
+app.post("/login", controllers.checkLogin)
+app.post("/user", controllers.getUser)
 
 app.listen(process.env.PORT, () => {
   console.log(`Сервер запущен по ссылке http://localhost:${process.env.PORT}`);
